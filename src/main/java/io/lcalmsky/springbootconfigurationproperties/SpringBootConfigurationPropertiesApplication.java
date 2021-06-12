3891fc7fe76b27f76720e710d2ede735301230d2
Jaime <lcalmsky@gmail.com>
2021-06-12 22:15:19 +0900

    Initial commit

---
+++ src/main/java/io/lcalmsky/springbootconfigurationproperties/SpringBootConfigurationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.springbootconfigurationproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigurationPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigurationPropertiesApplication.class, args);
+    }
+
+}
